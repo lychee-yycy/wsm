62ac40da31fa235b4ffb59096126508c3318ca1a
lychee-yycy <xiaoyaobing1994@gmail.com>
2024-10-26 19:59:45 +0800

    add wsm_pointer.capability
    Signed-off-by: lychee-yycy <xiaoyaobing1994@gmail.com>

--- input/wsm_pointer.c
+++ input/wsm_pointer.c
@@ -23,10 +23,10 @@ struct wsm_pointer *wsm_pointer_create(struct wsm_seat *seat,
 	pointer->pointer_wlr = wlr_pointer_from_input_device(wlr_device);
 	pointer->seat_device_wsm = device;
 	device->pointer = pointer;
-	
+
 	if (wlr_input_device_is_libinput(wlr_device)) {
-		struct udev_device *udev_device = 
-			libinput_device_get_udev_device(wlr_libinput_get_device_handle(wlr_device));
+		struct libinput_device *libinput_device = wlr_libinput_get_device_handle(wlr_device);
+		struct udev_device *udev_device = libinput_device_get_udev_device(libinput_device);
 		if (udev_device_get_property_value(udev_device, "ID_INPUT_MOUSE")) {
 			pointer->pointer_type = WSM_POINTER_TYPE_MOUSE;
 		} else if (udev_device_get_property_value(udev_device, "ID_INPUT_TOUCHPAD")) {
@@ -40,6 +40,33 @@ struct wsm_pointer *wsm_pointer_create(struct wsm_seat *seat,
 		} else {
 			pointer->pointer_type = WSM_POINTER_TYPE_UNKNOWN;
 		}
+
+		pointer->capability.supports_left_handed =
+			libinput_device_config_left_handed_is_available(libinput_device);
+		pointer->capability.supports_middle_emulation =
+			libinput_device_config_middle_emulation_is_available(libinput_device);
+		pointer->capability.supports_pointer_speed = true;
+		pointer->capability.supports_pointer_acceleration =
+			libinput_device_config_accel_is_available(libinput_device);
+		pointer->capability.support_pointer_acceleration_profiles = libinput_device_config_accel_get_profiles(libinput_device);
+		pointer->capability.supports_pointer_scroll_factor = true;
+		pointer->capability.supports_natural_scrol =
+			libinput_device_config_scroll_has_natural_scroll(libinput_device);
+
+		pointer->capability.supports_disable_while_typing =
+			libinput_device_config_dwt_is_available(libinput_device);
+		pointer->capability.supports_gesture =
+			libinput_device_has_capability(libinput_device, LIBINPUT_DEVICE_CAP_GESTURE);
+		pointer->capability.supports_calibration_matrix =
+			libinput_device_config_calibration_has_matrix(libinput_device);
+		pointer->capability.supports_disable_events =
+			libinput_device_config_send_events_get_modes(libinput_device) & LIBINPUT_CONFIG_SEND_EVENTS_DISABLED;
+		pointer->capability.supports_disable_events_on_external_mouse =
+			libinput_device_config_send_events_get_modes(libinput_device) & LIBINPUT_CONFIG_SEND_EVENTS_DISABLED_ON_EXTERNAL_MOUSE;
+		pointer->capability.supports_scroll_methods =
+			libinput_device_config_scroll_get_methods(libinput_device);
+		pointer->capability.supports_click_methods =
+			libinput_device_config_click_get_methods(libinput_device);
 	}
 
 	return pointer;
--- input/wsm_pointer.h
+++ input/wsm_pointer.h
@@ -1,6 +1,8 @@
 #ifndef WSM_POINTER_H
 #define WSM_POINTER_H
 
+#include <stdbool.h>
+
 struct wlr_pointer;
 
 struct wsm_seat;
@@ -18,6 +20,25 @@ enum wsm_pointer_type {
 struct wsm_pointer {
 	struct wlr_pointer *pointer_wlr;
 	struct wsm_seat_device *seat_device_wsm;
+
+	struct {
+		// mouse basic capability
+		bool supports_left_handed;
+		bool supports_middle_emulation;
+		bool supports_pointer_speed;
+		bool supports_pointer_acceleration;
+		bool support_pointer_acceleration_profiles;
+		bool supports_pointer_scroll_factor;
+		bool supports_natural_scrol;
+
+		bool supports_disable_while_typing;
+		bool supports_gesture;
+		bool supports_calibration_matrix;
+		bool supports_disable_events;
+		bool supports_disable_events_on_external_mouse;
+		bool supports_scroll_methods;
+		bool supports_click_methods;
+	} capability;
 
 	enum wsm_pointer_type pointer_type;
 };
